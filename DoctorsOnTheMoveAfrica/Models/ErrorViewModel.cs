34c2faa69c07f3b0be7a1e30d064a8b72b766efa
tizah <davidzagi93@gmail.com>
2019-08-15 14:12:24 +0100

    Add project files.

---
+++ DoctorsOnTheMoveAfrica/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DoctorsOnTheMoveAfrica.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
